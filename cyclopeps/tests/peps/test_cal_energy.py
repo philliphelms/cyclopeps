f50b559d45b09149d5ebd26fc81f53588e4842f1
Phillip Helms <phelms@caltech.edu>
2019-08-26 16:06:17 -0700

    Add 3x3 test

--- cyclopeps/tests/peps/test_cal_energy.py
+++ cyclopeps/tests/peps/test_cal_energy.py
@@ -141,17 +141,69 @@ class test_cal_energy(unittest.TestCase):
         norm0 = peps.calc_norm()
         print('Norm = {}'.format(norm0))
         # Perform the Exact energy calculation:
+        bra = einsum('abAcd,edBfg,ckDlm,fmEno->ABDE',peps[0][0],peps[0][1],peps[1][0],peps[1][1])
+        norm = einsum('ABDE,ABDE->',bra,conj(bra))
+        print('Full contract norm = {}'.format(norm))
+        E1_ = einsum('ABDE,abDE,ABab->',bra,conj(bra),ham[0][0][0])
+        E2_ = einsum('ABDE,ABde,DEde->',bra,conj(bra),ham[0][1][0])
+        E3_ = einsum('ABDE,aBdE,ADad->',bra,conj(bra),ham[1][0][0])
+        E4_ = einsum('ABDE,AbDe,BEbe->',bra,conj(bra),ham[1][1][0])
+        print(E1_,E2_,E3_,E4_)
         bra = einsum('LDWCM,lMXcu,CdYRm,cmZru->WXYZ',peps[0][0],peps[0][1],peps[1][0],peps[1][1])
         norm1 = einsum('WXYZ,WXYZ->',bra,conj(bra))
         E1  = einsum('WXYZ,wxYZ,WXwx->',bra,conj(bra),ham[0][0][0])
-        E1 += einsum('WXYZ,wXyZ,WYwy->',bra,conj(bra),ham[1][0][0])
-        E1 += einsum('WXYZ,WXyz,YZyz->',bra,conj(bra),ham[0][1][0])
-        E1 += einsum('WXYZ,WxYz,XZxz->',bra,conj(bra),ham[1][1][0])
+        E2  = einsum('WXYZ,WXyz,YZyz->',bra,conj(bra),ham[0][1][0])
+        E3  = einsum('WXYZ,wXyZ,WYwy->',bra,conj(bra),ham[1][0][0])
+        E4  = einsum('WXYZ,WxYz,XZxz->',bra,conj(bra),ham[1][1][0])
+        print(E1,E2,E3,E4)
+        E1 = E1+E2+E3+E4
         # Contract Energy again
         E2 = peps.calc_op(ham,normalize=False)
         print('Energy (exact)   = {}'.format(E1))
         print('Energy (routine) = {}'.format(E2))
         self.assertTrue(abs((E2-E1)/E1) < 1e-10)
+        mpiprint(0,'Passed\n'+'='*50)
+
+    def test_energy_itf_contraction_3x3(self):
+        mpiprint(0,'\n'+'='*50+'\nPeps Energy (Ham=ITF, 3x3) calculation\n'+'-'*50)
+        # Create a PEPS
+        from cyclopeps.tools.peps_tools import PEPS
+        Nx = 3
+        Ny = 3
+        d = 2
+        D = 3
+        peps = PEPS(Nx=Nx,Ny=Ny,d=d,D=D,chi=1000)
+        # Get the Hamiltonian
+        from cyclopeps.ops.itf import return_op
+        ham = return_op(Nx,Ny,(1.,2.))
+        # Calculate initial norm
+        norm0 = peps.calc_norm()
+        print('Norm = {}'.format(norm0))
+        # Perform the Exact energy calculation:
+        row1 = einsum('abAcd,edBfg,hgCij->ABCcfi',peps[0][0],peps[0][1],peps[0][2])
+        row2 = einsum('ckDlm,fmEno,ioFpq->cfiDEFlnp',peps[1][0],peps[1][1],peps[1][2])
+        row3 = einsum('lrGst,ntHuv,pvIwx->lnpGHI',peps[2][0],peps[2][1],peps[2][2])
+        bra = einsum('ABCcfi,cfiDEFlnp,lnpGHI->ABCDEFGHI',row1,row2,row3)
+        E1 = einsum('ABCDEFGHI,abCDEFGHI,ABab->',bra,conj(bra),ham[0][0][0])
+        E2 = einsum('ABCDEFGHI,AbcDEFGHI,BCbc->',bra,conj(bra),ham[0][0][1])
+        E3 = einsum('ABCDEFGHI,ABCdeFGHI,DEde->',bra,conj(bra),ham[0][1][0])
+        E4 = einsum('ABCDEFGHI,ABCDefGHI,EFef->',bra,conj(bra),ham[0][1][1])
+        E5 = einsum('ABCDEFGHI,ABCDEFghI,GHgh->',bra,conj(bra),ham[0][2][0])
+        E6 = einsum('ABCDEFGHI,ABCDEFGhi,HIhi->',bra,conj(bra),ham[0][2][1])
+        print(E1,E2,E3,E4,E5,E6)
+        E7 = einsum('ABCDEFGHI,aBCdEFGHI,ADad->',bra,conj(bra),ham[1][0][0])
+        E8 = einsum('ABCDEFGHI,ABCdEFgHI,DGdg->',bra,conj(bra),ham[1][0][1])
+        E9 = einsum('ABCDEFGHI,AbCDeFGHI,BEbe->',bra,conj(bra),ham[1][1][0])
+        E10= einsum('ABCDEFGHI,ABCDeFGhI,EHeh->',bra,conj(bra),ham[1][1][1])
+        E11= einsum('ABCDEFGHI,ABcDEfGHI,CFcf->',bra,conj(bra),ham[1][2][0])
+        E12= einsum('ABCDEFGHI,ABCDEfGHi,FIfi->',bra,conj(bra),ham[1][2][1])
+        print(E7,E8,E9,E10,E11,E12)
+        E = E1+E2+E3+E4+E5+E6+E7+E8+E9+E10+E11+E12
+        # Contract Energy again
+        E2 = peps.calc_op(ham,normalize=False)
+        print('Energy (exact)   = {}'.format(E))
+        print('Energy (routine) = {}'.format(E2))
+        self.assertTrue(abs((E2-E)/E) < 1e-10)
         mpiprint(0,'Passed\n'+'='*50)
 
 if __name__ == "__main__":
