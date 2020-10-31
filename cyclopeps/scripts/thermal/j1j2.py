98505919d7019ef377ae624cf9c279ad371444e6
Phillip Helms <phelms@caltech.edu>
2020-10-30 23:25:42 -0700

    Script updates

--- cyclopeps/scripts/thermal/j1j2.py
+++ cyclopeps/scripts/thermal/j1j2.py
@@ -14,14 +14,14 @@ dtind = int(argv[5])
 j2ind = int(argv[6])
 backend = 'numpy'
 d = 2
-peps_fdir = '/central/groups/changroup/members/phelms/fu_sweep/'
-peps_fname = '{}x{}_D{}_chi{}_dt{}_j2{}'.format(Nx,Ny,D,chi,dtind,j2ind)
+peps_fdir = '/central/groups/changroup/members/phelms/j1j2/heis'
+peps_fname = '{}x{}_D{}_chi{}_dt{}_j2{}_heis'.format(Nx,Ny,D,chi,dtind,j2ind)
 
 # TEBD Parameters
 step_size = [0.1,0.01][dtind]
 n_step     = int(5./step_size)
 truncate_loc = True
-local_chi = chi
+local_chi = int(2.*chi)
 
 # Get mpo
 j2 = [0.,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0][j2ind]
@@ -38,11 +38,12 @@ Ef,_ = run_tebd(Nx,
                 chiloc=local_chi,
                 backend=backend,
                 n_step=n_step,
-                als_iter=10,
-                als_tol=1e-5,
+                als_iter=20,
+                als_tol=1e-6,
                 truncate_loc=truncate_loc,
                 step_size=step_size,
                 peps_fdir=peps_fdir,
-                peps_fname=peps_fname)
+                peps_fname=peps_fname,
+                save_all_steps=True)
 
 print('\n\nFinal  E = {}'.format(Ef))
